6451f9c061cceb33c4d3a2b480a0c75343182576
xNtrow <xntrowdev@gmail.com>
2022-01-06 02:13:35 +0100

    Some Changes

--- src/main/java/com/axst/mixins/MixinMinecraft.java
+++ src/main/java/com/axst/mixins/MixinMinecraft.java
@@ -2,14 +2,27 @@ package com.axst.mixins;
 
 import com.axst.Client;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.DefaultResourcePack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.Util;
 import org.lwjgl.opengl.Display;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+
 @Mixin(Minecraft.class)
-public class MixinMinecraft {
+public abstract class MixinMinecraft {
+
+    @Shadow
+    @Final
+    private DefaultResourcePack mcDefaultResourcePack;
 
     @Inject(method = "createDisplay", at = @At("RETURN"))
     public void injectCreateDisplay(CallbackInfo ci){
@@ -26,4 +39,30 @@ public class MixinMinecraft {
     public void injectShutdownMinecraftApplet(CallbackInfo ci) {
         Client.onShutdown();
     }
+
+    /**
+     * @author Kaimson the Clown.
+     */
+    @Overwrite
+    private void setWindowIcon() {
+        if (Util.getOSType() != Util.EnumOS.OSX) {
+            try {
+                InputStream inputStream = MixinMinecraft.class.getResourceAsStream("/assets/minecraft/axst/icons/windows/icon16.png");
+                InputStream inputStream2 = MixinMinecraft.class.getResourceAsStream("/assets/minecraft/axst/icons/windows/icon32.png");
+                if (inputStream == null) {
+                    inputStream = this.mcDefaultResourcePack.getInputStreamAssets(new ResourceLocation("icons/icon_16x16.png"));
+                }
+                if (inputStream2 == null) {
+                    inputStream2 = this.mcDefaultResourcePack.getInputStreamAssets(new ResourceLocation("icons/icon_32x32.png"));
+                }
+                Display.setIcon(new ByteBuffer[] { this.readImageToBuffer(inputStream), this.readImageToBuffer(inputStream2) });
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Shadow
+    protected abstract ByteBuffer readImageToBuffer(final InputStream p0);
 }
